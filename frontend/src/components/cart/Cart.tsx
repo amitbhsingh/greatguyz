47936006fd5318d0c1a09e197059de3f65da806d
amitbhsingh <amit.bh.singh@gmail.com>
2023-11-16 15:22:37 -0500

    cart updated with useSelector

--- frontend/src/components/cart/Cart.tsx
+++ frontend/src/components/cart/Cart.tsx
@@ -1,11 +1,33 @@
 import './Cart.css'
-function Cart() {
-  return (
-    <div>
-      <h1 className="c-cart"> Cart </h1>
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store'; // Adjust the path to where your RootState is defined
 
+const Cart = () => {
+  // Use the useSelector hook to access the cart items from the Redux store
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map(item => (
+            <li key={item.id} className="cart-item">
+              <img src={item.image} alt={item.name} className="cart-item-image" />
+              <div className="cart-item-details">
+                <h3>{item.name}</h3>
+                <p>Quantity: {item.quantity}</p>
+                <p>Price: ₹{item.price}</p>
+                <p>Subtotal: ₹{item.price * item.quantity}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
