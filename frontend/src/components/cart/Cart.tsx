84c057ef5b64a5e13504a313233ebf2039a641c0
amitbhsingh <amit.bh.singh@gmail.com>
2023-11-19 17:46:01 -0500

    Increment Decrement functionality added, nanoid & date added to state

--- frontend/src/components/cart/Cart.tsx
+++ frontend/src/components/cart/Cart.tsx
@@ -2,7 +2,8 @@ import './Cart.css'
 import { useSelector } from 'react-redux';
 import {useDispatch} from 'react-redux'
 import { RootState } from '../../redux/store'; // Adjust the path to where your RootState is defined
-import {RemoveItem} from '../../features/auth/cartSlice'
+import {RemoveItem,IncreasePayload,DecreasePayload } from '../../features/auth/cartSlice'
+import Card from 'react-bootstrap/Card';
 
 
 const Cart = () => {
@@ -12,9 +13,18 @@ const Cart = () => {
   const taxRate=.28;
 
   const dispatch=useDispatch();
-  const removeItemFromCart=(id:number)=>{
+  const removeItemFromCart=(id: number)=>{
     dispatch(RemoveItem(id));
+    
   }
+  const increaseItem=(id:number)=>{
+    dispatch(IncreasePayload(id));
+  }
+  const decreaseItem=(id:number)=>{
+    dispatch(DecreasePayload(id));
+  }
+
+  // calculating total tax amount
   const totalTax = cartItems.reduce((acc, item) => {
     return acc + (item.price * item.quantity) * taxRate;
   }, 0);
@@ -46,8 +56,17 @@ const Cart = () => {
               
                 <p>Subtotal: ₹{item.price * item.quantity}</p>
                 <p>Tax: ₹{parseFloat(totalTax.toFixed(2))}</p>
-                <p>Total: ₹{totalPriceWithTax}</p>
-                <button className='cart1' onClick={() => removeItemFromCart(item.id)}>Remove</button>
+                
+                <button className='cartOneBtn' onClick={() => increaseItem(item.id)}>+</button>
+                <button className='cartOneBtn' onClick={() => removeItemFromCart(item.id)}>Remove</button>
+                <button className='cartOneBtn' onClick={() => decreaseItem(item.id)}>-</button>
+                <Card className='totalCart'style={{ width: '20rem'}}>
+                  <Card.Body>
+                    <h1><p>Total: ₹{parseFloat(totalPriceWithTax.toFixed(2))}</p></h1>
+                    <button className='cartOneBtn'><Card.Link href="/login">Order Now</Card.Link></button>
+                     <button className='cartOne'><Card.Link href="#">Checkout as Guest</Card.Link></button>
+                  </Card.Body>
+                  </Card>
               </div>
             </li>
           ))}
--- frontend/src/features/auth/cartSlice.ts
+++ frontend/src/features/auth/cartSlice.ts
@@ -1,11 +1,24 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice,PayloadAction } from '@reduxjs/toolkit';
+import {nanoid} from 'nanoid';
 
 interface CartItem {
   id: number;
+  cartItemId?: string;
   name: string;
   price: number;
   quantity: number;
   image: string;
+  addedAt?: Date;
+}
+
+interface IncreasePayload{
+  id: number;
+}
+interface DecreasePayload{
+  id: number;
+}
+interface RemoveItem{
+  id: number;
 }
 
 interface CartState {
@@ -20,20 +33,123 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    
     addItemToCart: (state, action: PayloadAction<CartItem>) => {
       const item = state.items.find(item => item.id === action.payload.id);
+    
       if (item) {
-        item.quantity += 1;
+        item.quantity +=action.payload.quantity ?? 0;
+        if (action.payload.price){
+          item.price=action.payload.price;
+        }
+        item.addedAt=new Date();
       } else {
-        state.items.push({ ...action.payload, quantity: 1 });
+        state.items.push({
+          ...action.payload,
+          quantity:action.payload.quantity ?? 1,
+          addedAt:new Date(),
+          cartItemId:nanoid(),
+        } as CartItem);
       }
     },
+    IncreasePayload: (
+      state,
+      action: PayloadAction<number>
+    ) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item){
+        item.quantity++;
+      }
+    },
+
+    DecreasePayload: (
+      state,
+      action: PayloadAction<number>
+    ) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity--;
+      }
+    },
+
+    
     RemoveItem: (state, action: PayloadAction<number>) => {
       state.items = state.items.filter(item => item.id !== action.payload);
     },
-    
   },
 });
 
-export const { addItemToCart, RemoveItem } = cartSlice.actions;
+export const { addItemToCart, RemoveItem, IncreasePayload, DecreasePayload } = cartSlice.actions;
 export default cartSlice.reducer;
+
+
+
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { nanoid } from 'nanoid';
+
+// interface CartItem {
+//   id: number;
+//   name: string;
+//   price: number;
+//   quantity: number;
+//   image: string;
+//   cartItemId: string; // Assuming you've added this and it is not optional
+//   addedAt?: string; // Assuming you've changed this from Date to string
+// }
+
+// interface CartState {
+//   items: CartItem[];
+// }
+
+// const initialState: CartState = {
+//   items: [],
+// };
+
+// const cartSlice = createSlice({
+//   name: 'cart',
+//   initialState,
+//   reducers: {
+//     addItemToCart: {
+//       reducer: (state, action: PayloadAction<CartItem>) => {
+//         const item = state.items.find(item => item.id === action.payload.id);
+//         if (item) {
+//           item.quantity += action.payload.quantity ?? 0;
+//           // Optionally update price or any other properties here
+//         } else {
+//           // Item not found, push a new one
+//           state.items.push({
+//             ...action.payload,
+//             quantity: action.payload.quantity ?? 1,
+//             cartItemId: action.payload.cartItemId ?? nanoid(),
+//             addedAt: action.payload.addedAt ?? new Date(),
+//           });
+//         }
+//       },
+//       prepare: (item: Omit<CartItem, 'cartItemId' | 'addedAt'>) => ({
+//         payload: {
+//           ...item,
+//           cartItemId: nanoid(), // Generate a unique ID for the cart item
+//           addedAt: new Date(),  // Set the timestamp when the item was added
+//         } as CartItem,
+//       }),
+//     },
+//     increaseQuantity: (state, action: PayloadAction<IncreasePayload>) => {
+//       const item = state.items.find((item) => item.id === action.payload.id);
+//       if (item) {
+//         item.quantity++;
+//       }
+//     },
+//     decreaseQuantity: (state, action: PayloadAction<DecreasePayload>) => {
+//       const item = state.items.find((item) => item.id === action.payload.id);
+//       if (item && item.quantity > 1) {
+//         item.quantity--;
+//       }
+//     },
+//     removeItem: (state, action: PayloadAction<RemovePayload>) => {
+//       state.items = state.items.filter(item => item.cartItemId !== action.payload.id);
+//     },
+//   },
+// });
+
+// export const { addItemToCart, removeItem, increaseQuantity, decreaseQuantity } = cartSlice.actions;
+// export default cartSlice.reducer;
