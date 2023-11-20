8b36905214bdf33eec74b12ee97a5680c93dbc3a
amitbhsingh <amit.bh.singh@gmail.com>
2023-11-19 22:07:48 -0500

    store updated with redux/persist and the data actually is persisting now

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,15 +1,32 @@
-import { configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
 import authReducer from '../features/auth/authSlice';
-import cartReducer from '../features/auth/cartSlice'
+import cartReducer from '../features/auth/cartSlice';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart'], // you might want to persist other reducers as well
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer:{
-    cart: cartReducer, 
-    auth: authReducer,
-    
-  }
-})
+  reducer: persistedReducer,
+  // middleware and other store enhancers can go here if you have any
+});
+
+export const persistor = persistStore(store);
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: { auth: AuthState, cart: CartState, ... }
+export type AppDispatch = typeof store.dispatch;
